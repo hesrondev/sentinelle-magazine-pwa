c2a834b0e7dadd553f83fb1d100c2cfa87a03e01
Louange BIZI <l.bizi@groupeonepoint.com>
2021-06-22 12:56:32 +0200

    redirect to new url website, update version, fix small phone title ellips

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   gtagTrackingCode: 'G-7BZF85CZ3N',
   baseUrl: 'https://api.sentinelle-magazine.com',
-  website: 'https://sentinelle-magazine.com'
+  website: 'https://sentinellemagazine.com'
 };
